33f3e49b094632825274407ce911c0d289edb0cd
Miraç Satıç <miracsa@yandex.com>
2015-07-19 18:43:28 +0300

    :q
    :!
    Views were finish

---
+++ main/migrations/0001_initial.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='User',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+                ('surname', models.CharField(max_length=200)),
+                ('email', models.EmailField(max_length=254)),
+                ('password', models.CharField(max_length=50)),
+            ],
+        ),
+    ]
