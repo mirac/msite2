eba5d8db950c07e8be66045025fb52bf274dca7a
Miraç Satıç <miracsa@yandex.com>
2015-07-19 18:08:01 +0300

    user model created

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    """User model class is here."""
+    name = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200)
+    email = models.EmailField()
+    password = models.CharField(max_length=50)
